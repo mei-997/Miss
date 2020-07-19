6b77011e552263f72f9e8412a0137a5245509208
mei-997 <68256628+mei-997@users.noreply.github.com>
2020-07-19 22:07:40 +0800

    1.11

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('a').click(function(){
+        //根据a标签的href转换为id选择器，获取id元素所处的位置，并高度减50px（这里根据需要自由设置）
+        $('html,body').animate({scrollTop: ($($(this).attr('href')).offset().top -100 )},1000);
+    });
+});
